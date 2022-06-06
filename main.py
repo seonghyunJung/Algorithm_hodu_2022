57f80188073679afb6bd9e153ab3d09ffd237292
Seonghyun Jung <tjdgusus@gmail.com>
2022-06-06 18:44:26 +0900

    algorithm1.py 및 main_2.py 오류 수정

--- main.py
+++ main.py
@@ -5,11 +5,11 @@
 
 
 class Machine:
-    isUsing = False# 사용중인지?
-    usingTime = 10# 사용하는데 필요한 초
-    remainingTime = usingTime# 사용 끝날때까지 남은 시간
+    isUsing = False  # 사용중인지?
+    usingTime = 10  # 사용하는데 필요한 초
+    remainingTime = usingTime  # 사용 끝날때까지 남은 시간
 
-    def __init__(self, usingTime, remainingTime = 0):
+    def __init__(self, usingTime, remainingTime=0):
         self.usingTime = usingTime
         self.remainingTime = remainingTime
 
@@ -22,16 +22,17 @@ class Machine:
             self.remainingTime = self.usingTime
             return overUse
 
+
 class Beverage:
     name = ""
-    recipe = [] # 레시피, 실행 시간
-    human_or_machine = [] # 사람이 하는일인지 기계가 하는일인지, 사람은 True, 기계는 false
+    recipe = []  # 레시피, 실행 시간
+    human_or_machine = []  # 사람이 하는일인지 기계가 하는일인지, 사람은 True, 기계는 false
 
     def __init__(self, name,  recipe, human_or_machine):
         self.name = name
         self.recipe = recipe
         self.human_or_machine = human_or_machine
-    
+
     def lenRecipe(self):
         return len(self.recipe)
 
@@ -41,15 +42,16 @@ class MachineController:
     machineBlender = []
     machines = [machineEspresso, machineBlender]
 
-    def find(self, machineKind:int):
+    def find(self, machineKind: int):
         for machine in self.machines[machineKind]:
             if machine.isUsing == False:
                 self.sendBackward(machineKind)
                 return machine
         return None
 
-    def sendBackward(self, machineKind:int):
+    def sendBackward(self, machineKind: int):
         self.machines[machineKind].append(self.machines[machineKind].pop(0))
+
 
 class Person:
     order = []
@@ -57,8 +59,6 @@ class Person:
 
     def do(self):
         pass
-
-
 
 
 # if __name__ == '__main__':
