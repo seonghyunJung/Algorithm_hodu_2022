c2fb12953093edceacb329525c76e0830af1892c
Yuyeon-Kim <1rladbdus@gmail.com>
2022-06-12 00:24:56 +0900

    실행 시간을 줄이기 위해 print문 제거

--- algorithm3.py
+++ algorithm3.py
@@ -15,7 +15,8 @@ class Recorder: # 알고리즘 평가 지표 기록 클래스
         self.beverage_average_making_time_list = [] # case 별 음료가 만들어지는 시간의 평균 기록
         self.beverage_average_complete_time_list = [] # case 별 음료가 완성된 시간의 평균 기록
         self.team_average_complete_time_list = [] # case 별 팀 음료가 모두 완성된 시간의 평균 기록
-    
+        self.average = []
+
     def record(self, all_complete_time:int, waited_time:int, beverage_start_time:list, beverage_complete_time:list, team_len:list):
         self.all_complete_time_list.append(all_complete_time)# case 별 모든 작업이 끝날 때까지 걸린 시간 기록
         self.waited_time_list.append(waited_time)# case 별 기다린 시간 기록
@@ -43,17 +44,30 @@ class Recorder: # 알고리즘 평가 지표 기록 클래스
         dic["beverage_average_making_time_list"] = self.beverage_average_making_time_list
         dic["beverage_average_complete_time_list"] = self.beverage_average_complete_time_list
         dic["team_average_complete_time_list"] = self.team_average_complete_time_list
+        dic["average of all_complete_time,waited_time,beverage_average_making_time,beverage_average_complete_time,team_average_complete_time"]=self.average
         with open('./algorithm3_data.json','w') as f:
-            json.dump(dict, f, indent=4) # 한글 포함된 경우, ensure_ascii=False 옵션 추가
+            json.dump(dic, f, indent=4) # 한글 포함된 경우, ensure_ascii=False 옵션 추가
 
-        
+    def print_average(self):
+        self.average = [np.mean(self.all_complete_time_list), 
+        np.mean(self.waited_time_list), 
+        np.mean(self.beverage_average_making_time_list),
+        np.mean(self.beverage_average_complete_time_list),
+        np.mean(self.team_average_complete_time_list) ]
+
+        print("all_complete_time", self.average[0])  
+        print("waited_time", self.average[1])  
+        print("beverage_average_making_time", self.average[2])
+        print("beverage_average_complete_time", self.average[3])
+        print("team_average_complete_time", self.average[4])
 
 
  # 객체 생성
-machineController = MachineController([Machine("에스프레소 머신1", 24), Machine("에스프레소 머신2", 24)],
-                                          [Machine("블렌더1", 30), Machine("블렌더2", 30)],
-                                          [Machine("티 우리기1", 300), Machine("티 우리기2", 300), Machine("티 우리기3", 300), Machine("티 우리기4", 300), Machine("티 우리기5", 300)])
-person = Person(machineController)
+# machineController = machineController
+# MachineController([Machine("에스프레소 머신1", 24), Machine("에스프레소 머신2", 24)],
+#                                           [Machine("블렌더1", 30), Machine("블렌더2", 30)],
+#                                           [Machine("티 우리기1", 300), Machine("티 우리기2", 300), Machine("티 우리기3", 300), Machine("티 우리기4", 300), Machine("티 우리기5", 300)])
+
 
 
 file_path = "Testing_data(정상 주문)/testing_data(1만(정상)).txt"
@@ -75,12 +89,13 @@ temp = []
 for t, c in zip(team, case):
 
     order = [copy.deepcopy(bevList[j]) for j in c]
-    print("[제작할 음료]", end=" ")
-    for beverage in order:
-        print(beverage.name, end=", ")
-    print()
+    # print("[제작할 음료]", end=" ")
+    # for beverage in order:
+        # print(beverage.name, end=", ")
+    # print()
 
     # 변수 초기화
+    person = Person(machineController)
     current_idx = 0 # 실행중 인덱스 초기화
     check = -1 # 무한반복 방지 기계 꽉참 표시 변수
 
@@ -96,15 +111,15 @@ for t, c in zip(team, case):
     beverage_complete_time = [-1]*len(order) # 음료 제작을 완료한 시간
 
     while done != len(order): # 주문이 있는 동안 반복
-        print()
-        print("[진행중인 음료 수]", len(doing_list))
-        print("[완료한 음료 수]", done)
-        print("[남은 손님 수]", len(order)-done)
-        print("[현재 음료 정보]", order[current_idx].name)
-        print("[현재 시간]", person.usedTime)
+        # print()
+        # print("[진행중인 음료 수]", len(doing_list))
+        # print("[완료한 음료 수]", done)
+        # print("[남은 손님 수]", len(order)-done)
+        # print("[현재 음료 정보]", order[current_idx].name)
+        # print("[현재 시간]", person.usedTime)
         res = person.do(order[current_idx]) # 위치의 인덱스 실행
         if res == 2: # 완료한 경우
-            print("[제작 완료]", order[current_idx].name)                
+            # print("[제작 완료]", order[current_idx].name)                
             order[current_idx] = None
             # order.pop(current_idx) # 해당 인덱스의 값 pop
             beverage_complete_time[current_idx] = person.usedTime # 끝난 시간 저장 # TODO: 끝난 시간 저장 오류 수정
@@ -122,7 +137,7 @@ for t, c in zip(team, case):
                     while order[temp] == None or temp in doing_list: # temp<len(order) and 
                         temp += 1
                     doing_list.append(temp) # 진행중인 리스트에 새로운 인덱스 추가
-                    print("작업수를 ", len(doing_list),"로 증가")
+                    # print("작업수를 ", len(doing_list),"로 증가")
                 else:
                     break
             # continue
@@ -155,7 +170,7 @@ for t, c in zip(team, case):
             continue
 
         elif res == 0: # 해당 작업을 실행할 수 없는 경우
-            print("[제작 실패]", end = " ")
+            # print("[제작 실패]", end = " ")
 
             if check == current_idx: # 다른 작업을 진행해봤으나, 더 진행이 안되는 경우
                 if len(doing_list)<(len(order)-done):# 진행중인 작업 개수 늘릴 수 있을 때
@@ -163,38 +178,38 @@ for t, c in zip(team, case):
                     while order[temp] == None or temp in doing_list: # temp<len(order) and 
                         temp += 1
                     doing_list.append(temp) # 진행중인 리스트에 새로운 인덱스 추가
-                    print("작업수를 ", len(doing_list),"로 증가")
+                    # print("작업수를 ", len(doing_list),"로 증가")
 
                 else:
                     f = min(machineController.getEarlistEnd(i+1) for i in range(len(machineController.machines))) # 가장 빨리 끝나는 기계 시간 # 가장 짧은 시간 계산
                     waited_time += f # 기다린 시간 추가
                     check = -1 # 기계 꽉참 해제
-                    print("모든 작업이 실행 불가능, 가장 빨리 끝나는 기계가 끝날 때까지",f,"초 기다림")
+                    # print("모든 작업이 실행 불가능, 가장 빨리 끝나는 기계가 끝날 때까지",f,"초 기다림")
                     person.timeFlow(f) # 기계 남은 시간 중 가장 짧은 시간만큼 기다리기
                     continue
 
             elif check == -1: # 작업을 더이상 늘릴 수 없고, 다른 작업을 진행해볼 여지가 있을 때
-                print("다른 작업이 실행 가능한지 탐색")
+                # print("다른 작업이 실행 가능한지 탐색")
                 check = current_idx # 할 일 : 사람이 할 수 있는 일 있는지 보고, 없으면 기계 기다리기...?
 
             # 실행 중 인덱스 업데이트
         
         doing_idx = (doing_idx + 1) % len(doing_list) # 진행중 음료 개수로 나눈 나머지 값으로 업데이트 하여 동시에 제작하는 음료 수 제한
         current_idx = doing_list[doing_idx] # 진행할 인덱스를 진행중인 리스트에서 찾아서 설정
-        print("[음료 변경]", order[current_idx].name)
+        # print("[음료 변경]", order[current_idx].name)
 
-    print()
-    print("=========================== 실행 결과 ==========================")
-    print("총 걸린 시간:",person.usedTime)
-    print("모든 기계 실행중인 동안 기다린 시간:", waited_time)
-    print("음료 제작을 시작한 시간:",beverage_start_time)
-    print("음료가 나오기까지 걸린 시간:",beverage_complete_time)
-    print("================================================================")
+    # print()
+    # print("=========================== 실행 결과 ==========================")
+    # print("총 걸린 시간:",person.usedTime)
+    # print("모든 기계 실행중인 동안 기다린 시간:", waited_time)
+    # print("음료 제작을 시작한 시간:",beverage_start_time)
+    # print("음료가 나오기까지 걸린 시간:",beverage_complete_time)
+    # print("================================================================")
 
     record.record(person.usedTime, waited_time, beverage_start_time, beverage_complete_time, t)
 
+record.print_average()
 record.save_to_file()
-
 
 # =========================== 실행 결과 ==========================
 # 총 걸린 시간: 572
